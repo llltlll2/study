d644a009e5da82153f7f2e823f842cab0386b402
llltlll2 <40252679+llltlll2@users.noreply.github.com>
2024-09-11 07:43:49 +0900

    study.py の更新

--- python/study.py
+++ python/study.py
@@ -6,3 +6,13 @@ li = [i * 2 for i in range(10)]
 
 argparse
 コマンドライン引数の取得
+
+statistics
+.mean() データの平均
+.mediam() データの中央値
+.variance() データの不偏分散
+
+urllib
+urlから値を持ってくる
+.request().urlopen() 
+色々拾えるっぽい？調査
